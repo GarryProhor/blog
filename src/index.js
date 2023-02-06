5b3c7e930492944dbee45ed9b105ac5b99123873
igorp <igorprohorchenko@gmail.com>
2023-02-06 15:38:03 +0300

    Title

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AuthProvider } from './context/authContext';
+import { SidebarProvider } from './context/sidebarContext';
+import { BlogsProvider } from './context/blogsContext';
+import { UserProvider } from './context/userContext';
+import { CommentProvider } from './context/commentContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AuthProvider>
+        <SidebarProvider>
+            <UserProvider>
+                <BlogsProvider>
+                    <CommentProvider>
+                        <App />
+                    </CommentProvider>
+                </BlogsProvider>
+            </UserProvider>
+        </SidebarProvider>
+    </AuthProvider>
 );
