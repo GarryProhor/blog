8051ea453833499f7883eb942f24b3c1fae91446
igorp <igorprohorchenko@gmail.com>
2023-02-06 15:12:32 +0300

    route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.scss';
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Home, Login, SingleBlog} from "./pages";
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <BrowserRouter>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path = "/login" element = {<Login />} />
+          <Route path = "/" element = {<Home />} />
+          <Route path = "/blog/:id" element = {<SingleBlog />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
   );
 }
 
